0c029dcff866ee07f9390c1b937aa03b690711c3
FlyRideR1986 <31609725+FlyRideR1986@users.noreply.github.com>
2025-11-02 18:24:35 +0800

    Update mas.ps1

--- mas.ps1
+++ mas.ps1
@@ -1,4 +1,3 @@
 $proxy="site2.861228.xyz"
 (Invoke-RestMethod "https://$proxy/raw.githubusercontent.com/FlyRideR1986/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd").Replace("://","://$proxy/") | Set-Content MAS.bat -Encoding UTF8 -Force
 cmd.exe /c MAS.bat
-Remove-item MAS.bat -Force

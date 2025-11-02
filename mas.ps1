9869599c6286cfb6f85250c72ab558af3dc357ca
FlyRideR1986 <31609725+FlyRideR1986@users.noreply.github.com>
2025-11-02 18:23:37 +0800

    Refactor MAS script to use Invoke-RestMethod
    Updated the script to use Invoke-RestMethod and added cleanup.

--- mas.ps1
+++ mas.ps1
@@ -1,3 +1,4 @@
 $proxy="site2.861228.xyz"
-
-(irm "https://$proxy/raw.githubusercontent.com/FlyRideR1986/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd").Replace("://","://$proxy/") | Set-Content MAS.bat -Encoding UTF8 -Force; cmd.exe /c MAS.bat
+(Invoke-RestMethod "https://$proxy/raw.githubusercontent.com/FlyRideR1986/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd").Replace("://","://$proxy/") | Set-Content MAS.bat -Encoding UTF8 -Force
+cmd.exe /c MAS.bat
+Remove-item MAS.bat -Force

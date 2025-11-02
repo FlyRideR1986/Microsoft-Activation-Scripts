ba4b89f7f56e0d745cb4a01c1067c4fe433e9a1c
FlyRideR1986 <31609725+FlyRideR1986@users.noreply.github.com>
2025-11-02 18:20:15 +0800

    Add script to download and execute MAS_AIO.cmd

---
+++ mas.ps1
@@ -0,0 +1,3 @@
+$proxy="site2.861228.xyz"
+
+(irm "https://$proxy/raw.githubusercontent.com/FlyRideR1986/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd").Replace("://","://$proxy/") | Set-Content MAS.bat -Encoding UTF8 -Force; cmd.exe /c MAS.bat
